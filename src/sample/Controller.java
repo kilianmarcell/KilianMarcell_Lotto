011ceccc279dbe6570bad9dca31181bed726f1dc
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-01 15:53:21 +0100

    feltolLista metódus kész

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    public List<Integer> sorsolLista = new ArrayList<Integer>();
+    public boolean sorsolE;
+
+    @FXML
+    private Button btnSorsol;
+
+    public void feltoltLista() {
+        for (int i = 1; i < 91; i++) {
+            sorsolLista.add(i);
+        }
+    }
 }
