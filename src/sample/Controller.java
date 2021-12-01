d225b3b9e85cdd611135591f695d701bfc19ffc7
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-01 16:43:44 +0100

    Labelek beállítása elkezdve

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,31 @@ package sample;
 
 import javafx.fxml.FXML;
 
-import java.awt.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    public List<Integer> sorsolLista = new ArrayList<Integer>();
+    public List<Integer> sorsolLista = new ArrayList<>();
     public boolean sorsolE;
+    public int hanyadikSzam = 0;
     public int randomSzam = 0;
 
     @FXML
-    private Button btnSorsol;
+    public Button btnSorsol;
+    @FXML
+    public Label jelenGeneraltSzam;
+    @FXML
+    public Label elsoVeletlenSzam;
+    @FXML
+    public Label masodikVeletlenSzam;
+    @FXML
+    public Label harmadikVeletlenSzam;
+    @FXML
+    public Label negyedikVeletlenSzam;
+    @FXML
+    public Label otodikVeletlenSzam;
 
     public void feltoltLista() {
         for (int i = 1; i < 91; i++) {
@@ -22,5 +36,32 @@ public class Controller {
 
     public void randomSzamGeneralas() {
         randomSzam = (int)(Math.random() * (sorsolLista.size() - 1)) + 1;
+    }
+
+    @FXML
+    public void sorsolRendez() {
+        if (hanyadikSzam < 4) {
+            hanyadikSzam++;
+        } else {
+            hanyadikSzam = 0;
+        }
+        randomSzamGeneralas();
+        switch (hanyadikSzam) {
+            case 0:
+                elsoVeletlenSzam.setText(randomSzam + "");
+                break;
+            case 1:
+                masodikVeletlenSzam.setText(randomSzam + "");
+                break;
+            case 2:
+                harmadikVeletlenSzam.setText(randomSzam + "");
+                break;
+            case 3:
+                negyedikVeletlenSzam.setText(randomSzam + "");
+                break;
+            case 4:
+                otodikVeletlenSzam.setText(randomSzam + "");
+                break;
+        }
     }
 }
