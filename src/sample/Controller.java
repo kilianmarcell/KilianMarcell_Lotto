57a7f3d54b002bf5111f7d8edaf950b2d32f8aae
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-02 11:30:09 +0100

    Sorsolás hibásan működik, TODO: ismétlődő sorsolt számok, sorsolásnál rendezés

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,12 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Controller {
     public List<Integer> veletlenLista = new ArrayList<>();
     public List<Integer> sorsoltLista = new ArrayList<>();
-    public boolean sorsolE;
     public int hanyadikSzam = 0;
     public int randomSzam = 0;
 
@@ -42,8 +42,8 @@ public class Controller {
 
     public void randomSzamGeneralas() {
         randomSzam = (int)(Math.random() * (veletlenLista.size() - 1)) + 1;
-        veletlenLista.remove(randomSzam);
-        sorsoltLista.add(veletlenLista.get(randomSzam));
+        veletlenLista.remove(randomSzam - 1);
+        sorsoltLista.add(randomSzam);
     }
 
     @FXML
@@ -66,15 +66,30 @@ public class Controller {
                 otodikVeletlenSzam.setText(randomSzam + "");
                 break;
         }
-        if (hanyadikSzam < 4) {
+        if (hanyadikSzam == 0) {
+            elsoVeletlenSzam.setText(randomSzam + "");
+            masodikVeletlenSzam.setText("");
+            harmadikVeletlenSzam.setText("");
+            negyedikVeletlenSzam.setText("");
+            otodikVeletlenSzam.setText("");
             hanyadikSzam++;
-        } else {
-            sorsolE = false;
+        } else if (hanyadikSzam > 0 && hanyadikSzam < 4) {
+            hanyadikSzam++;
+        } else if (hanyadikSzam == 4) {
             btnSorsol.setText("Rendez");
+            hanyadikSzam++;
+        } else if (hanyadikSzam == 5) {
+            Collections.sort(sorsoltLista);
+            elsoVeletlenSzam.setText(sorsoltLista.get(0) + "");
+            masodikVeletlenSzam.setText(sorsoltLista.get(1) + "");
+            harmadikVeletlenSzam.setText(sorsoltLista.get(2) + "");
+            negyedikVeletlenSzam.setText(sorsoltLista.get(3) + "");
+            otodikVeletlenSzam.setText(sorsoltLista.get(4) + "");
+            sorsoltLista.clear();
+            veletlenLista.clear();
+            feltoltLista();
             hanyadikSzam = 0;
-        }
-        if (!sorsolE) {
-
+            btnSorsol.setText("Sorsol");
         }
     }
 }
